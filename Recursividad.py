2ea6ae45435b5d93156af3cc40ba71a303801ab2
Daniel88V <daniel12velo@gmail.com>
2025-07-21 19:48:16 -0600

    Recursividad-menu

--- Recursividad.py
+++ Recursividad.py
@@ -1,13 +1,4 @@
 """
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-n = int(input("Ingrese número: "))
-print(factorial(n))
-"""
-"""
 def potencia (base, exponente):
     if exponente == 0:
         return 1
@@ -27,3 +18,18 @@ print("5. Invertir cadena de texto")
 print("6. Calcular potencia")
 print("7. Salir")
 print("Seleccione una opción")
+opcion = input()
+if opcion == "1":
+    def factorial(n):
+        if n == 0:
+            return 1
+        else:
+            return n * factorial(n - 1)
+    n = int(input("Ingrese número: "))
+    print(factorial(n))
+elif opcion == "2":
+    def naturales(num):
+        if num == 0:
+            return 1
+        else:
+            return num + naturales(num-1)
