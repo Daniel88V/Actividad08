a72ab260bf3c64bd2ea1fbe36f56b6533bdea505
Daniel88V <daniel12velo@gmail.com>
2025-07-21 19:31:27 -0600

    Recursividad-menu

---
+++ Recursividad.py
@@ -0,0 +1,21 @@
+"""
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n = int(input("Ingrese número: "))
+print(factorial(n))
+"""
+"""
+def potencia (base, exponente):
+    if exponente == 0:
+        return 1
+    else:
+        return base *potencia(base, exponente-1)
+base = int(input("Ingrese base: "))
+exponente = int(input("Ingrese exponente: "))
+print(potencia(base, exponente))
+"""
+
+print("======BIENVENIDO AL MENÚ======")
